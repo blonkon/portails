95c334905c24181706a83d1baac91de64fd8f3ba
Lonpo <boss@gmail.com>
2023-07-03 11:46:29 +0000

    preparestatement

--- ajout.php
+++ ajout.php
@@ -1,6 +1,23 @@
-<?php include_once("database.php");
-$sql = "select * from `personne`"; 
-$requete = $db->query($sql);
-$user = $requete->fetch();
-var_dump($user);
+<?php 
+//connexion a la database
+include_once("database.php");
+
+//requete 
+$sql = "INSERT INTO `apprenant(nom,prenom,age,naissance,email,tel,promotion,certification)` 
+VALUES
+(:nom,  :prenom,    :age,   :naissance,    :email, :tel,   :promotion,    :certification)"; 
+
+//preparons la requete
+$requete = $db->prepare($sql);
+
+// Injection de parametre
+$requete->bindParam(":nom",$nom,PDO::PARAM_STR);
+$requete->bindParam(":prenom",$prenom,PDO::PARAM_STR);
+$requete->bindParam(":age",$age,PDO::PARAM_INT);
+$requete->bindParam(":naissance",$naissance,PDO::PARAM_STR);
+$requete->bindParam(":email",$email,PDO::PARAM_STR);
+$requete->bindParam(":tel",$tel,PDO::PARAM_INT);
+$requete->bindParam(":promotion",$promotion,PDO::PARAM_INT);
+$requete->bindParam(":certification",$certification,PDO::PARAM_STR);
+
 ?>
