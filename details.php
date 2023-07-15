0bfc46a3951095b8fff3bc7fb71fb3d9d8ecdfbb
Lonpo <boss@gmail.com>
2023-07-15 12:28:02 +0000

    Details_finish

--- details.php
+++ details.php
@@ -9,44 +9,52 @@
         <style>
             main{
                 display: grid;
-                grid-template-columns: 1fr 1fr 1fr 1fr;
+                grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
             }
             .left-side{
                 grid-column-start: 3;
-                grid-column-end: span 2;
+                grid-column-end: span 3;
                 margin-top: 0;
             }
             .left-side h1{
                 color: white;
                 font-size: 30px;
-                margin-left: 30%;
+                margin-left: 34%;
 
             }
             .left-side{
                 margin-top: 20%;
             }
             .box{
-                width: 80%;
-                display: grid;
-                grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+                width: 85%;
                 color: black;
                 background-color: white;
                 border: 1px solid #F38712;
-                padding: 3%;
+                padding: 4%;
                 border-radius: 20px;
             }
+            .box-content{
+                display: grid;
+                grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr ;
+            }
             .value{
-                grid-column-start: 3;
-                grid-column-end: span 3;
+                grid-column-start: 5;
+                grid-column-end: span 4;
                 font-size: 100px;
                 letter-spacing: 1px;
             }
             .items{
+                grid-column-start: 2;
                 font-weight: bold;
-            }             
+            }
+            .button-div{
+                width: 100%;
+            }          
+            .button-div a{
+               margin-left: 44%;
+            }
             button{
                 cursor: pointer;
-                text-align: center;
                 width: auto;
                 padding: 5px;
                 height: 30px;
@@ -64,16 +72,46 @@
                 cursor: pointer;
             }
             .items > p{
-                font-size: 20px;
+                font-size: 15px;
                 letter-spacing: normal;
                 font-family:'Montserrat';
+                padding-bottom: 15px;
             }
             .value > p{
-                font-size: 20px;
+                font-size: 15px;
                 letter-spacing: normal;
                 font-family:'Montserrat';
-
+                padding-bottom: 15px;
             }
+            .profile{
+                width: 100%;
+                margin-bottom: 30px;
+            }
+            .profile img{
+                border: 1px solid #F38712;
+                border-radius: 50%;
+                margin-left: 40%;
+                width: 100px;
+                cursor: pointer;
+                height: 100px;
+            }
+            #container{
+                position: absolute;
+                width: 100%;
+                height: 100%;
+                top: 0;
+                left: 0;
+                background-color: rgba(20, 20, 20, 0.9);
+                text-align: center;
+                display: none;
+            }
+            #fullscreen{
+                padding: 2%;
+                max-width: 98%;
+                max-height: 98%;
+                cursor: pointer;
+            }
+
         </style>
     </head>
     <body>
@@ -91,34 +129,44 @@
             <div class="right-side">
                 <img src="image/main.png">
             </div>
-            <div class="left-side">
+            <div class="left-side" style="margin-top: 6%;">
                 <h1>Information</h1>
                     <div  class="box">
-                    <!-- Affichage de la liste  -->
-                    <div class="items">
-                        <p class="items1">Matricule</p>
-                        <p class="items2">Prenom</p>
-                        <p class="items3">Nom</p>
-                        <p class="items4">Age</p>
-                        <p class="items5">Naissance</p>
-                        <p class="items6">Numero</p>
-                        <p class="items7">Email</p>
-                        <p class="items8">Promotion</p>
-                        <p class="items9">Certification</p>
+                        <div class="profile">
+                            <!-- <div> -->
+                                <img src="image/main.png" onclick="plein(this.src)">
+                            <!-- </div> -->
+                        </div>
+                    <div class="box-content">
+                        <div class="items">
+                            <p class="items1">Matricule</p>
+                            <p class="items2">Prenom</p>
+                            <p class="items3">Nom</p>
+                            <p class="items4">Age</p>
+                            <p class="items5">Naissance</p>
+                            <p class="items6">Numero</p>
+                            <p class="items7">Email</p>
+                            <p class="items8">Promotion</p>
+                            <p class="items9">Certification</p>
+                        </div>
+                
+                        <div class="value">
+                            <p class="value1">12332KD</p>
+                            <p class="value2">Ibrahim</p>
+                            <p class="value3">Diakite</p>
+                            <p class="value4">24</p>
+                            <p class="value5">2/22/2002</p>
+                            <p class="value6">77890965</p>
+                            <p class="value7">Diakkkgazaeeaazzezeezazz@gmail.com</p>
+                            <p class="value8">Promotion 3</p>
+                            <p class="value9">02/12/2023</p>
+                        </div>
                     </div>
-            
-                    <div class="value">
-                        <p class="value1">12332KD</p>
-                        <p class="value2">Ibrahim</p>
-                        <p class="value3">Diakite</p>
-                        <p class="value4">24</p>
-                        <p class="value5">2/22/2002</p>
-                        <p class="value6">77890965</p>
-                        <p class="value7">Diakkkgz@gmail.com</p>
-                        <p class="value8">Promotion 3</p>
-                        <p class="value9">02/12/2023</p>
-                    </div><br><br>
-                    <div class="button-div"><a href="fiche.php"><button>Modifier</button></a></div>
+                    <div class="button-div"><a href="fiche_modif.php"><button>Modifier</button></a></div>
+                        <!--        Plein ecran gestion              -->
+                        <div id="container" >
+                            <img id="fullscreen" onclick="fermer()">
+                        </div>
                         <?php 
                         $page = 2;
                         // include_once("databaseliste.php");
@@ -131,5 +179,14 @@
             Copyright &copy; 2023
             <br><br><br>
         </footer>
+        <script>
+            function plein(lien){
+                document.getElementById("fullscreen").src = lien;
+                document.getElementById("container").style.display = "block";
+            }
+            function fermer(){
+                document.getElementById("container").style.display = "none";
+            }
+        </script>
     </body>
 </html>
