73d7a8e3c8ccf37e97c8f6591caccbc72693bf0b
lonpo <dncper@gmail.com>
2023-07-16 21:49:20 +0200

    home

--- databaseliste.php
+++ databaseliste.php
@@ -14,7 +14,7 @@
     require_once("database.php");
 
     //requete sql
-    $sql="Select nom,prenom,promotion,images from apprenant order by prenom asc";
+    $sql="Select nom,prenom,promotion,images,id from apprenant order by prenom asc";
 
     //execute la requete
     $requete = $db->query($sql);
@@ -26,11 +26,14 @@
 
 <?php foreach($apprenants as $apprenant):  ?>
     <a href="#" class="infos">
+        <script>
+            var id = <?php echo $apprenant["id"]; ?>;
+        </script>
         <li class="li">
             <?php
             $nam = $apprenant["images"];
             $source = "files/$nam";
-            echo('<img src="'.$source.'">');
+            echo('<img style="width: 32px;height: 32px;border-radius: 5px;" src="'.$source.'">');
             ?>
             <span class="span1">
                 <b><?= strip_tags($apprenant["prenom"])."   ".strip_tags($apprenant["nom"]) ?></b>
@@ -55,7 +58,23 @@
                            window.location.href = 'modif.php';
                         }
                         function delet(){
-                            window.location.href = "del.php";
+                            alert(id);
+                            // Créer une instance de l'objet XMLHttpRequest
+                            var xhr = new XMLHttpRequest();
+
+                            // Définir le type de requête et l'URL du script PHP
+                            xhr.open("POST", "del.php", false);
+
+                            // Définir l'en-tête de la requête si nécessaire
+                            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+                            // Préparer les données à envoyer
+                            var data = "elementid=" + encodeURIComponent(id);
+
+                            // Envoyer la requête avec les données
+                            xhr.send(data);
+                            //pour rafraichir la page vu que c'est une requete ajax donc elle s'execute en arriere plan
+                            location.reload();
                         }
                     </script>
 
---
+++ del.php
@@ -0,0 +1,33 @@
+<script>alert("OK");</script>
+<?php
+// la variable page est de savoir si la requete viens d'une page de mon site et non de l'url sinon on retourne chez l'index  
+        if(!isset($_SESSION)) 
+       { 
+           session_start(); 
+       }
+    if (!isset($_SESSION["admin"])) {
+        header("location: index.php");
+        exit;
+    }   
+    // connexion a la database
+    require_once("database.php");
+    $id = $_POST["elementid"];
+    //requete sql
+    $sql = "select images from apprenant where id = $id";
+    //execute la requete
+    //effacons le fichier sur le serveur
+    $requete = $db->query($sql);
+    $fichier = $requete->fetch();
+    $img = $fichier["images"];
+    unlink("files/$img");
+
+
+    $sql="DELETE FROM `apprenant` WHERE `apprenant`.`id` = $id";
+
+    //execute la requete
+    $requete = $db->query($sql);
+
+    // //recuperer les donnees 
+    // $apprenants = $requete->fetchAll();
+    header("location:liste.php");
+?>
